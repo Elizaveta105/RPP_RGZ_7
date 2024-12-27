f6942ef7d8e470bbc4d1b73df107b0ec16698fea
Elizaveta105 <124755383+Elizaveta105@users.noreply.github.com>
2024-12-27 20:33:41 +0700

    Редактирование файла

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 class Config:
-    DB_NAME = "RPP_7_RGZ"
-    DB_USER = "liza_u_nastya"
-    DB_PASSWORD = "password"
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
     DB_HOST = "localhost"
-    DB_PORT = 5432
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'your_secret_key'
+    SECRET_KEY = os.getenv('SECRET_KEY')
